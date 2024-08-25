1c6249ab28c9dede6535a56cc52b021446bdf5b2
Leonardo Scommegna <leonardo.scommegna@unifi.it>
2024-08-25 12:25:33 +0200

    cleaning

--- src/main/java/unifi/stlab/SteadyStateAnalysis.java
+++ src/main/java/unifi/stlab/SteadyStateAnalysis.java
@@ -45,12 +45,12 @@ public class SteadyStateAnalysis {
 
                 PoolRejuvenationBuilder uncoordinatedBuilder = new UncoordinatedRejuvenationBuilder();
                 PoolRejuvenationBuilder sequentialbuilder = new SequentialCoordinatedRejuvenationBuilder();
-                PoolRejuvenationBuilder parallelbuilder = new ParallelCoordinatedRejuvenationBuilder();
+                // PoolRejuvenationBuilder parallelbuilder = new ParallelCoordinatedRejuvenationBuilder();
 
                 ArrayList<PoolRejuvenationBuilder> models = new ArrayList<>();
                 models.add(uncoordinatedBuilder);
                 models.add(sequentialbuilder);
-                models.add(parallelbuilder);
+                // models.add(parallelbuilder);
 
                 int leftBound = 5;
                 int rightBound = 5000;
@@ -107,62 +107,6 @@ public class SteadyStateAnalysis {
         }
 
 
-
-
-//                String filePath =  builder.getNickName() + "SteadyState-triggerSearch" + formattedDate + ".csv";
-//
-//
-//                createCsv(filePath, "trigger,Ko,Rej" );
-//
-//
-//
-//                //String rewards = "Ko;Ko+Rej";
-//                String unavailabilityReward = builder.getUnavailabilityReward();
-//                String unreliabilityReward = builder.getUnreliabilityReward();
-//                //String unreliabilityReward = builder.getModulatedUnreliabilityReward(2);
-//                //String unreliabilityReward = builder.getNumberedUnreliabilityReward(2);
-//
-//
-//                String rewards = unavailabilityReward + ";" + unreliabilityReward;
-//                //String rewards = "Ko;Ko+RejPool";
-//
-//
-//                RegSteadyState analysis = RegSteadyState.builder().build();
-//
-//
-//                SteadyStateSolution<Marking> result;
-//                SteadyStateSolution<RewardRate> resultReward;
-//                Map<RewardRate, BigDecimal> steadyState;
-//
-//
-//                for(int waitTrigger = 100; waitTrigger<=10000; waitTrigger+=100){
-//
-//                        builder.changeTrigger(net,Integer.toString(waitTrigger));
-//                        result = analysis.compute(net, marking);
-//                        resultReward = SteadyStateSolution.computeRewards(result, rewards);
-//                        steadyState = resultReward.getSteadyState();
-//
-//                        BigDecimal reliability = steadyState.entrySet().stream().filter(t -> t.getKey().toString().equals(unreliabilityReward)).findFirst().get().getValue();
-//                        //BigDecimal availability = steadyState.entrySet().stream().filter(t -> t.getKey().toString().equals("Ko+Rej")).findFirst().get().getValue();
-//                        BigDecimal availability = steadyState.entrySet().stream().filter(t -> t.getKey().toString().equals(unavailabilityReward)).findFirst().get().getValue();
-//
-//                        addRow(filePath, waitTrigger, reliability, availability);
-//
-//                        System.out.println("Steady-state probabilities:");
-//                        for ( RewardRate m : steadyState.keySet()) {
-//                                System.out.printf("%1.6f -- %s%n", steadyState.get(m), m);
-//                        }
-//
-//                }
-
-
-//        public static void changeTrigger(PetriNet net, String triggerValue){
-//                Transition trigger = net.getTransition("trigger");
-//                trigger.removeFeature(StochasticTransitionFeature.class);
-//                trigger.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal(triggerValue), MarkingExpr.from("1", net)));
-//        }
-
-
         public static void createCsv(String filePath, String cols) {
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
                         // Scrive l'intestazione del file CSV
@@ -182,22 +126,5 @@ public class SteadyStateAnalysis {
                         e.printStackTrace();
                 }
         }
-
-
-//                // transient until time=12, error 0.005 (per epoch), integration step=0.02
-//                RegTransient analysis = RegTransient.builder()
-//                        .greedyPolicy(new BigDecimal("10"), new BigDecimal("0"))
-//                        .timeStep(new BigDecimal("0.05")).build();
-//
-//                TransientSolution<DeterministicEnablingState, Marking> solution =
-//                        analysis.compute(net, marking);
-//
-//
-//                String rewards = "Rej;Ok;Err;Ko";
-//                TransientSolution<DeterministicEnablingState, RewardRate> solutionRewards = TransientSolution.computeRewards(false, solution, rewards);
-//
-//                // display transient probabilities
-//                new TransientSolutionViewer(solutionRewards);
-        //double[][][] solutionArray = solutionRewards.getSolution();
 }
 
